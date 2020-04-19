8c91bf44da12b103c5c1636fa7e613a0449ce383
Ysmael Braga <ysmaelbraga01@gmail.com>
2020-04-19 03:38:59 -0300

    Foi feito o js do menu e do scroll,tambem foi feito a parte de mudar o img do background.

---
+++ js/modules/menu-mobile.js
@@ -0,0 +1,69 @@
+import outsideClick from'./outsideclick.js'
+export default function initMenuMobile(){
+  const menuButton = document.querySelector('.hamburguer');
+  const menuList = document.querySelector('.ul-menu');
+  const eventos = ['click'];
+
+  if(menuButton) {
+
+    function openMenu(event) {
+      menuList.classList.add('ativo');
+      menuButton.classList.add('ativo');
+      outsideClick(menuList, eventos, () => {
+        menuList.classList.remove('ativo');
+        menuButton.classList.remove('ativo');
+      })
+
+    }
+
+    const mostraAviso = document.querySelector('.entre-contato');
+    
+  
+ 
+    if(window.innerWidth <=839) {
+      menuButton.style.display = 'flex';
+    }if(window.innerWidth >839) {
+      menuButton.style.display = 'none';
+      menuList.style.display = 'flex';
+    }
+ function moveScroll(event) {
+  const {window} = event.path[1];
+  setInterval(() => {
+  if(window.scrollY > 0 && window.innerWidth >0) {
+        mostraAviso.classList.add('ativo');
+        menuList.style.display = 'none';
+        menuButton.style.display = 'none';
+        
+  }
+  if(window.scrollY <=0) {
+    mostraAviso.classList.remove('ativo');
+    menuList.style.display = 'block';
+    menuButton.style.display = 'flex';
+ 
+  }
+
+  if(window.scrollY <=0 && window.innerWidth <=839) {
+     menuButton.style.display = 'flex';
+    
+  }
+  if(window.scrollY <=0 && window.innerWidth >840) {
+    menuButton.style.display = 'none';
+   
+  }
+},0);
+  
+ }
+
+document.addEventListener('scroll', moveScroll);
+
+    eventos.forEach((evento) => {
+      menuButton.addEventListener(evento, openMenu);
+    })
+
+  }
+}
+
+
+
+
+
---
+++ js/script.js
@@ -0,0 +1,5 @@
+import initMenuMobile from  './modules/menu-mobile.js';
+import initmudaImagem from  './modules/muda-imagem.js';
+
+initMenuMobile();
+initmudaImagem();
